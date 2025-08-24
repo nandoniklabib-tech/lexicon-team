02e6797550c2dea46c2daeb93e795fa67e163f8d
arman9022 <arman.hub.bd@gmail.com>
2025-08-24 14:28:51 +0600

    Update listening.blade.php

--- resources/views/Backend/MockTest/TestPage/listening.blade.php
+++ resources/views/Backend/MockTest/TestPage/listening.blade.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>LEXICON - IELTS - Listening</title>
+    <title>123 LEXICON - IELTS - Listening</title>
 
     <!-- favicon -->
     <link rel="shortcut icon" href="" type="image/x-icon">
