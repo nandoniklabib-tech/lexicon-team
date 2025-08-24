17ca115041670cd77bb12cf067b7093e70b7d2d0
nandoniklabib-tech <nandoniklabib@gmail.com>
2025-08-24 16:29:44 +0600

    abc

--- database/sql3.sql
+++ database/sql3.sql
@@ -1,2 +1,4 @@
-INSERT INTO reading_passages (question_group_id, title, content, created_at, updated_at)
-VALUES (5, 'Part 1 <br>Reading Passage 1', 'You should spend about 20 minutes on <b>Questions 1 -13</b>, which are based on Reading Passage 1 below.', NOW(), NOW());
+INSERT INTO questions (question_group_id, text, type, order_no, question_no, meta_data, created_at, updated_at)
+VALUES
+(5, '<b>Questions 1-4</b><br>The text has 5 paragraphs (A - E).<br>Which paragraph contains each of the following pieces of information?.', 'static', 1, NULL, NULL, NOW(), NOW());
+
