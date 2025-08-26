f35e8f8be06b1f03cb3b0422b5c8bf30c1c54de7
arman9022 <arman.hub.bd@gmail.com>
2025-08-26 14:25:46 +0600

    march question upload sql3

---
+++ database/armanTEST.sql
@@ -0,0 +1,3 @@
+INSERT INTO questions (question_group_id, text, type, order_no, question_no, meta_data, created_at, updated_at)
+VALUES
+(1, 'TEXT', 'static', SERIAL_NO, NULL, NULL, NOW(), NOW());
---
+++ database/sql3.sql
@@ -0,0 +1,53 @@
+INSERT INTO questions (question_group_id, text, type, order_no, question_no, meta_data, created_at, updated_at)
+VALUES
+(1, '<b>Questions 1-7</b> <br>Complete the form below.<br>Write NO MORE THAN THREE WORDS AND/OR A NUMBER for each answer.', 'static', 1, NULL, NULL, NOW(), NOW());
+
+INSERT INTO questions (question_group_id, text, type, order_no, question_no, meta_data, created_at, updated_at)
+VALUES
+(1, '<b>Details of Order</b>', 'static', 2, NULL, NULL, NOW(), NOW());
+
+INSERT INTO questions (question_group_id, text, type, order_no, question_no, meta_data, created_at, updated_at)
+VALUES
+(1, 'Party Host', 'static', 3, NULL, '{"table_no":1,"row":1,"col":1}', NOW(), NOW()),
+(1, 'Example: Customer`s oldest daughter', 'static', 4, NULL, '{"table_no":1,"row":1,"col":2}', NOW(), NOW()),
+(1, 'Occasion', 'static', 5, NULL, '{"table_no":1,"row":2,"col":1}', NOW(), NOW()),
+(1, '18th birthday', 'static', 6, NULL, '{"table_no":1,"row":2,"col":2}', NOW(), NOW()),
+(1, 'Number of Guests Invited', 'static', 7, NULL, '{"table_no":1,"row":3,"col":1}', NOW(), NOW()),
+(1, ' ___', 'fill_blank', 8, 1, '{"table_no":1,"row":3,"col":2}', NOW(), NOW()),
+(1, 'Customer Budget', 'static', 9, NULL, '{"table_no":1,"row":4,"col":1}', NOW(), NOW()),
+(1, '___ £800', 'fill_blank', 10, 2, '{"table_no":1,"row":4,"col":2}', NOW(), NOW()),
+(1, 'Marquee Size', 'static', 11, NULL, '{"table_no":1,"row":5,"col":1}', NOW(), NOW()),
+(1, '___ X 9 metres', 'fill_blank', 12, 3, '{"table_no":1,"row":5,"col":2}', NOW(), NOW()),
+(1, '___ Cost', 'fill_blank', 13, 4, '{"table_no":1,"row":6,"col":1}', NOW(), NOW()),
+(1, '£450', 'static', 14, NULL, '{"table_no":1,"row":6,"col":2}', NOW(), NOW()),
+(1, '___', 'fill_blank', 15, 5, '{"table_no":1,"row":7,"col":1}', NOW(), NOW()),
+(1, '£150', 'static', 16, NULL, '{"table_no":1,"row":7,"col":2}', NOW(), NOW()),
+(1, 'Lighting Cost', 'static', 17, NULL, '{"table_no":1,"row":8,"col":1}', NOW(), NOW()),
+(1, 'approximately ₤ ___', 'fill_blank', 18, 6, '{"table_no":1,"row":8,"col":2}', NOW(), NOW()),
+(1, 'Number of Guests', 'static', 19, NULL, '{"table_no":1,"row":9,"col":1}', NOW(), NOW()),
+(1, 'seated 30 / standing 50', 'static', 20, NULL, '{"table_no":1,"row":9,"col":2}', NOW(), NOW()),
+(1, 'Furniture Cost', 'static', 21, NULL, '{"table_no":1,"row":10,"col":1}', NOW(), NOW()),
+(1, 'per table £4.00 / per chair £3.00', 'static', 22, NULL, '{"table_no":1,"row":10,"col":2}', NOW(), NOW()),
+(1, 'Dates Marquee Required', 'static', 23, NULL, '{"table_no":1,"row":11,"col":1}', NOW(), NOW()),
+(1, 'Set-up date ___ To be taken down on June 7th', 'fill_blank', 24, 7, '{"table_no":1,"row":11,"col":2}', NOW(), NOW());
+
+INSERT INTO questions (question_group_id, text, type, order_no, question_no, meta_data, created_at, updated_at)
+VALUES
+(1, '<b>Questions 8-10</b> <br>Complete the form below.<br>Write NO MORE THAN THREE WORDS AND/OR A NUMBER for each answer.', 'static', 25, NULL, NULL, NOW(), NOW());
+
+INSERT INTO questions (question_group_id, text, type, order_no, question_no, meta_data, created_at, updated_at)
+VALUES
+(1, '<b>Customer Details</b>', 'static', 26, NULL, NULL, NOW(), NOW());
+
+INSERT INTO questions (question_group_id, text, type, order_no, question_no, meta_data, created_at, updated_at)
+VALUES
+(1, 'Name', 'static', 27, NULL, '{"table_no":2,"row":1,"col":1}', NOW(), NOW()),
+(1, '___', 'fill_blank', 28, 8, '{"table_no":2,"row":1,"col":2}', NOW(), NOW()),
+(1, 'Postcode', 'static', 29, NULL, '{"table_no":2,"row":2,"col":1}', NOW(), NOW()),
+(1, '___', 'fill_blank', 30, 9, '{"table_no":2,"row":2,"col":2}', NOW(), NOW()),
+(1, 'Contact Number', 'static', 31, NULL, '{"table_no":2,"row":3,"col":1}', NOW(), NOW()),
+(1, '___', 'fill_blank', 32, 10, '{"table_no":2,"row":3,"col":2}', NOW(), NOW());
+
+INSERT INTO questions (question_group_id, text, type, order_no, question_no, meta_data, created_at, updated_at)
+VALUES
+(2, '<b>Questions 11-15</b><br>Label the map below of a typical medieval castle.<br>Write the correct letter, A-G, next to questions 11-15.', 'static', 33, NULL, NULL, NOW(), NOW());
